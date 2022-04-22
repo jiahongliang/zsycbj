517977c3d4f27ebe444655e1a5941162555d3f50
贾洪亮 <hljia@msn.com>
2022-04-22 10:46:18 +0800

    optimize loading time

--- webpage/src/main.js
+++ webpage/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import {Pagination,Carousel,CarouselItem} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import store from './store'
 import App from './App.vue'
 
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
+Vue.use(Pagination)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
 
 Vue.config.productionTip = false
 
